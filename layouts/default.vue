9b8f37d02bb01fa5be29ff58d1cb80b74f7339a7
Nojiniy <nonkit3418@yahoo.co.jp>
2019-07-01 13:29:49 +0900

    初期コミット

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,43 @@
 <template>
   <div>
+    <header class="header">
+      <p class="header__logo">
+        ポートフォリオ
+      </p>
+      <ul class="gnav">
+        <li class="gnav__item">
+          <a href="#profile" class="gnav__link">プロフィール</a>
+        </li>
+      </ul>
+      <ul class="gnav">
+        <li class="gnav__item">
+          <a href="#skill" class="gnav__link">スキルセット</a>
+        </li>
+      </ul>
+      <ul class="gnav">
+        <li class="gnav__item">
+          <a href="" class="gnav__link">仕事の相談</a>
+        </li>
+      </ul>
+    </header>
     <nuxt />
+    <footer class="footer">
+      <ul class="footer-nav">
+        <li class="footer-nav__item">
+          <a href="#profile" class="footer-nav__link">プロフィール</a>
+        </li>
+        <li class="footer-nav__item">
+          <a href="#skill" class="footer-nav__link">スキルセット</a>
+        </li>
+        <li class="footer-nav__item">
+          <a href="#contact" class="footer-nav__link">仕事の相談</a>
+        </li>
+      </ul>
+      <p class="footer__copyright">
+        &copy; 2019.
+      </p>
+    </footer>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<style></style>
